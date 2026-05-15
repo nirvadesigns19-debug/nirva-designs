711889f46f6b14a63bb8eb924b3a182d5acb470a
nirvadesigns19-debug <nirvadesigns19@gmail.com>
2026-05-15 10:18:03 +0530

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,1 +1,43 @@
+const signupForm =
+  document.getElementById("signupForm");
 
+signupForm.addEventListener(
+  "submit",
+  function(e){
+
+    e.preventDefault();
+
+    const name =
+      document.getElementById(
+        "signupName"
+      ).value;
+
+    const email =
+      document.getElementById(
+        "signupEmail"
+      ).value;
+
+    const password =
+      document.getElementById(
+        "signupPassword"
+      ).value;
+
+    const user = {
+      name,
+      email,
+      password
+    };
+
+    localStorage.setItem(
+      "nirvaUser",
+      JSON.stringify(user)
+    );
+
+    alert(
+      "Account created successfully!"
+    );
+
+    window.location.href =
+      "login.html";
+
+});
