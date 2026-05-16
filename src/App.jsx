fa342fab01e7495582bc1ac9b1f4a91c6659c9c3
nirvadesigns19-debug <nirvadesigns19@gmail.com>
2026-05-16 15:35:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ export default function App() {
   return (
     <div
       style={{
-        width: "100%",
-        height: "100vh",
         backgroundColor: "black",
         color: "#D4AF37",
+        width: "100%",
+        height: "100vh",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
