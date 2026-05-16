389593a1e1fbcf257fb24ce1e25a650fb3b02840
nirvadesigns19-debug <nirvadesigns19@gmail.com>
2026-05-16 15:00:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
+function App() {
+  return (
+    <div
+      style={{
+        background: "#000",
+        color: "#D4AF37",
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "40px",
+        fontWeight: "bold",
+      }}
+    >
+      NIRVA DESIGNS
+    </div>
+  );
+}
 
+export default App;
